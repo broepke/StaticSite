0e82d6eb4eec9d26fcba7eae1830cbadc6f4e433
Brian Roepke <brian.roepke@autodesk.com>
2021-09-13 00:06:26 -0700

    Minor Edits

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.roepkeb.com'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
@@ -20,6 +20,5 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
-
 GOOGLE_ANALYTICS = 'UA-3093'
 DISQUS_SITENAME = "roepkeb"
