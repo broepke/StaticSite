7e99d8b1d7d1f4bc6e0d31e1883309192962f1e3
Brian Roepke <broepke@gmail.com>
2021-11-28 07:51:34 -0800

    Moved to Data Knows All

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,9 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Brian Roepke'
-SITENAME = 'roepkeb'
-SITEURL = 'https://www.roepkeb.com'
+SITENAME = 'Data Knows All'
+SITEURL = 'https://www.dataknowsall.com'
+
 # https://analytics.google.com/analytics/web
 GOOGLE_ANALYTICS = 'UA-309309-9'
 # https://disqus.com/admin/
@@ -16,16 +17,13 @@ RELATIVE_URLS = False
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = True
 USE_FOLDER_AS_CATEGORY = True 
-PATH = 'content'
-ARTICLE_PATHS = ['posts',]
-PAGE_PATHS = ['pages',]
-MENUITEMS = (
-    ('Résumé', '/pdf/cv.pdf'),
-    ('Portfolio', '/pages/portfolio.html'),
-    ('About', '/pages/about.html'),
-    # ('Blog', '/blog_index.html'),
-    # ('Blog Home', '/index.html'),
-)
+DEFAULT_PAGINATION = 6
+
+# https://github.com/gilsondev/pelican-clean-blog/
+THEME='theme'
+COLOR_SCHEME_CSS = 'tomorrow.css'
+HEADER_COVER = 'images/home.jpg'
+
 TIMEZONE = 'America/Los_Angeles'
 DEFAULT_LANG = 'en'
 # INDEX_SAVE_AS = 'blog_index.html'
@@ -36,16 +34,26 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# https://github.com/gilsondev/pelican-clean-blog/
-THEME='theme'
-COLOR_SCHEME_CSS = 'tomorrow.css'
-HEADER_COVER = 'images/home.jpg'
+PATH = 'content'
+ARTICLE_PATHS = ['posts',]
+PAGE_PATHS = ['pages',]
+PLUGIN_PATHS=['pelican-plugins']
+PLUGINS = ['render_math', 'gravatar']
+
+MENUITEMS = (
+    ('Résumé', '/pdf/cv.pdf'),
+    ('Portfolio', '/pages/portfolio.html'),
+    ('About', '/pages/about.html'),
+    # ('Blog', '/blog_index.html'),
+    # ('Blog Home', '/index.html'),
+)
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/broepke'),
-          ('github', 'https://github.com/broepke'),
-          ('linkedin','https://www.linkedin.com/in/broepke/'))
-
+SOCIAL = (
+    ('twitter', 'https://twitter.com/broepke'),
+    ('github', 'https://github.com/broepke'),
+    ('linkedin','https://www.linkedin.com/in/broepke/')
+)
 
 # static paths will be copied without parsing their contents
 STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'pdf', 'other']
@@ -53,8 +61,3 @@ EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
     'extra/favicon.ico': {'path': 'favicon.ico'}
 }
-
-DEFAULT_PAGINATION = 6
-
-PLUGIN_PATHS=['pelican-plugins']
-PLUGINS = ['render_math', 'gravatar']
